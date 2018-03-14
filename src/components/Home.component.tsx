e950dfdd98ace82dde79388bf937890c84884b00
Bennett Hardwick (PC) <bennetthardwick@icloud.com>
2018-03-14 20:11:42 +1000

    Added link to tree on home view'

--- src/components/Home.component.tsx
+++ src/components/Home.component.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom'
 
 export class Home extends React.Component {
     render() {
-        return <h1>Home!</h1>
+        return <div>
+          <h1>Home!</h1>
+          <Link to="/tree">Go to tree</Link>
+        </div>
     }
 }
