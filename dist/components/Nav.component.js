cacdaf3b5a6bd47317e2d79497317e47f60e3394
Bennett Hardwick <bennetthardwick@icloud.com>
2018-03-22 15:13:42 +1000

    Removed compiled files

--- dist/components/Nav.component.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-class Nav extends React.Component {
-    render() {
-        return React.createElement("h1", null, "This is the nav");
-    }
-}
-exports.Nav = Nav;
