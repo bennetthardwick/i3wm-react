c6059c19d3a47a0c013f3204aec8a920e45d64f8
Bennett Hardwick <bennetthardwick@icloud.com>
2018-03-22 16:55:25 +1000

    Used humour to generate a funny way of saying that the red square is a terminal

--- src/components/i3/terminal.component.tsx
+++ src/components/i3/terminal.component.tsx
@@ -6,6 +6,6 @@ let terminalStyle = {
 
 export class Terminal extends React.Component<any, any> {
   render() {
-    return <div style={{ width: this.props.width, height: this.props.height, ...terminalStyle }} key={this.props.id}>It's a: {this.props.type}</div>
+    return <div style={{ width: this.props.width, height: this.props.height, ...terminalStyle }} key={this.props.id}>This is a {this.props.type} or something</div>
   }
 }
